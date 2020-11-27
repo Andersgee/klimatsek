c43b1137a82f2ee4a2830886b04306dbc93b5c18
Anders Gustafsson <andersgee@gmail.com>
2020-11-27 15:53:52 +0100

    deleted stuff

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import React from "react";
+import PropTypes from "prop-types";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline">
+  <div>
     {gridItems.map((item) => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: '240px',
-                display: 'inline-block',
-              }}
-            >
-              <PreviewCompatibleImage imageInfo={item} />
-            </div>
+      <div key={item.text}>
+        <section>
+          <div
+            style={{
+              width: "240px",
+              display: "inline-block",
+            }}
+          >
+            <PreviewCompatibleImage imageInfo={item} />
           </div>
+
           <p>{item.text}</p>
         </section>
       </div>
     ))}
   </div>
-)
+);
 
 FeatureGrid.propTypes = {
   gridItems: PropTypes.arrayOf(
@@ -31,6 +30,6 @@ FeatureGrid.propTypes = {
       text: PropTypes.string,
     })
   ),
-}
+};
 
-export default FeatureGrid
+export default FeatureGrid;
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function Index() {
+  return (
+    <Layout>
+      <div>
+        <h1>this is the aboutpage</h1>
+        <p>paragraph.</p>
+      </div>
+    </Layout>
+  );
+}
